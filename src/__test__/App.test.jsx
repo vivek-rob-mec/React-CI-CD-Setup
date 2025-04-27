8d9fdfc2099bb905b409d3d3e5e5e02c76be2b42
vivek-rob-mec <viveksaroj098@gmail.com>
2025-04-27 18:13:55 +0530

    Added codes

--- src/__test__/App.test.jsx
+++ src/__test__/App.test.jsx
@@ -1,6 +1,6 @@
 // src/__test__/App.test.js
 
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from '../App';
 import { Component } from "react";
 import { expect } from "vitest";
